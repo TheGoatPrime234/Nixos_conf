52a01e9844597ff7cb6ae806b2eb1d89dd9f6c04
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-04-17 20:42:58 +0200

    Adapted rebuild to new repo conditions

--- rebuild.sh
+++ rebuild.sh
@@ -4,21 +4,6 @@ set -e
 cd ~/nixos-config
 
 git add . 
-cd ~/.config
-git add btop/
-git add cava/
-git add hypr/
-git add kitty/
-git add nvim/
-git add spicetify/
-git add swaync/
-git add syncthing/
-git add waybar/
-git add wlogout/
-git add yazi/
-git add rofi/
-cd ~/nixos-config
-echo "Starte Rebuild für Host:"
 sudo nixos-rebuild switch --flake .#nixos -v
 
 if [ -z "$1" ]; then
