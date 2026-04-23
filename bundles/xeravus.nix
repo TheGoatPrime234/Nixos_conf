9465f9b28717e9980110f66ae8f89681c1fcec59
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-04-23 20:56:34 +0200

    Changes for the Nixmodul Parser

--- bundles/xeravus.nix
+++ bundles/xeravus.nix
@@ -16,27 +16,20 @@
     ./modules/cybersecurity.nix
     ./modules/vault.nix
     ./../modules/ani-cli.nix
+    ./../modules/audio.nix
     ./../modules/bluetooth.nix
-    ./../modules/boot.nix
     ./../modules/btop.nix
-    ./../modules/dbus.nix
     ./../modules/direnv.nix
-    ./../modules/git.nix
     ./../modules/gnome-keyring.nix
     ./../modules/gparted.nix
-    ./../modules/libnotify.nix
     ./../modules/nvidia.nix
-    ./../modules/playerctl.nix
-    ./../modules/sl.nix
     ./../modules/tailscale.nix
     ./../modules/tree.nix
     ./../modules/vscode.nix
     ./../modules/wget.nix
     ./../modules/zip.nix
-    ./../modules/nixvim.nix
     ./../modules/zen-browser.nix
     ./../modules/yazi.nix
-    ./../modules/zsh.nix
     ./../modules/spicetify.nix
   ];
 
@@ -44,18 +37,20 @@
     xanterella = {
       local.enable = true;
       ani-cli.enable = true;
+      audio.enable = true;
       bluetooth.enable = true;
       btop.enable = true;
       direnv.enable = true;
       gnome-keyring.enable = true;
       gparted.enable = true;
       nvidia.enable = true;
-      audio.enable = true;
       tailscale.enable = true;
       tree.enable = true;
       vscode.enable = true;
       wget.enable = true;
       zip.enable = true;
+      zen-browser.enable = true;
+      yazi.enable = true;
       spicetify.enable = true;
     };
   };
