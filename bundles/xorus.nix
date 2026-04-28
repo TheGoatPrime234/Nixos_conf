16b60aaf71ca355582efdbb2d5cef54d71eac4b0
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-04-28 22:47:35 +0200

    changes

--- bundles/xorus.nix
+++ bundles/xorus.nix
@@ -7,30 +7,24 @@
 }: {
   imports = [
     ./../modules/local.nix
-    ./modules/essentials.nix
-    ./modules/nixedit.nix
-    ./modules/laptop.nix
-    ./modules/hyprland.nix
-    inputs.nixvim.nixosModules.default
-    ./../modules/audio.nix
-    ./../modules/bluetooth.nix
     ./../modules/btop.nix
     ./../modules/direnv.nix
-    ./../modules/tailscale.nix
-    ./../modules/tree.nix
-    ./../modules/wget.nix
-    ./../modules/zip.nix
-    ./../modules/yazi.nix
+    ./../modules/ssh.nix
+    ./../modules/git.nix
+    ./../modules/boot.nix
+    ./../modules/colmena.nix
+    ./modules/hyprland.nix
   ];
 
   config = {
     xanterella = {
       local.enable = true;
+      boot.enable = true;
+      btop.enable = true;
       direnv.enable = true;
-      tailscale.enable = true;
-      tree.enable = true;
-      wget.enable = true;
-      yazi.enable = true;
+      ssh.enable = true;
+      colmena.enable = true;
+      git.enable = true;
     };
   };
 }
