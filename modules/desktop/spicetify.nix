577a10e0fc443115496cd7c0e7e2006e871c859d
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-04-30 21:28:51 +0200

    Auto-Rebuild: 2026-04-30 21:28:51

--- modules/desktop/spicetify.nix
+++ modules/desktop/spicetify.nix
@@ -6,6 +6,22 @@
   ...
 }: let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+  spotify-fix = pkgs.symlinkJoin {
+    name = "spotify-fix";
+    pname = "spotify";
+    version = pkgs.spotify.version;
+    meta = pkgs.spotify.meta;
+    paths = [
+      pkgs.spotify
+    ];
+    buildInputs = [
+      pkgs.makeWrapper
+    ];
+    postBuild = ''
+      wrapProgram $out/bin/spotify \
+      --add-flags "--disable-gpu"
+    '';
+  };
 in {
   imports = [
     inputs.spicetify-nix.nixosModules.default
@@ -17,6 +33,7 @@ in {
   config = lib.mkIf config.xanterella.spicetify.enable {
     programs.spicetify = {
       enable = true;
+      spotifyPackage = spotify-fix;
       enabledExtensions = with spicePkgs.extensions; [
         hidePodcasts
         shuffle
