7dccfdafdaff189a9b60ecf5b7bf398bd1513b37
Xeravus <cato.jenisch@gmail.com>
2026-06-20 15:00:20 +0200

    Auto-Rebuild: 2026-06-20 15:00:20

--- modules/agenix/secrets.nix
+++ modules/agenix/secrets.nix
@@ -1,11 +1,14 @@
 let
   cato = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHc0eOrLgxwDdvrFC9WEtOsh+Sx5AqZUUKxhrQWaPIPE cato.jenisch@gmail.com";
-
-  # Übersicht
   users = [
     cato
   ];
+
+  xeravus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzkvv1qjg8y//HRhcyEoZ7luoVhBNgqvJ1HIzceAVu1";
+  lutik = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFxZS/IOSqNcBYlg1oCDUIzqURCiNoQTmdXXQKcSfMpY";
   systems = [
+    xeravus
+    lutik
   ];
 in {
   "global.age" = {
