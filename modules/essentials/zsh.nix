3d02131a0b29690bb545391a8f8dac4308974dab
Xeravus <cato.jenisch@gmail.com>
2026-07-20 13:59:46 +0200

    Auto-Rebuild: 2026-07-20 13:59:46

--- modules/essentials/zsh.nix
+++ modules/essentials/zsh.nix
@@ -48,49 +48,36 @@
           sv = "sudo nvim";
           za = "yazi";
           nix-pr = "nixpkgs-review pr --print-result";
+          gst = "git status";
+          gck = "git checkout";
+          gcm = "git commit -am";
+          gbr = "git branch";
+          gdf = "git diff --stat";
         };
         interactiveShellInit = ''
-                              ZSH_CACHE_DIR="$HOME/.cache/zsh"
-                                      if [[ ! -d "$ZSH_CACHE_DIR" ]]; then
-                                        mkdir -p "$ZSH_CACHE_DIR"
-                                      fi
-                         export ZSH_COMPDUMP="$ZSH_CACHE_DIR/zcompdump-$HOST-$ZSH_VERSION"
-                         if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-                         source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-                      fi
-                                        source ${inputs.p10k-src}/powerlevel10k.zsh-theme
-                                               [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+          ZSH_CACHE_DIR="$HOME/.cache/zsh"
+          if [[ ! -d "$ZSH_CACHE_DIR" ]]; then
+            mkdir -p "$ZSH_CACHE_DIR"
+          fi
 
-                                               # Deine Yazi-Funktion y()
-                                               function y() {
-                                                 local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-                                                 command yazi "$@" --cwd-file="$tmp"
-                                                 IFS= read -r -d "" cwd < "$tmp"
-                                                 [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
-                                                 rm -f -- "$tmp"
-                                               }
-          function git() {
-              if [[ "$1" == "ck" ]]; then
-          	shift
-          	command git checkout "$@"
-                     elif [[ "$1" == "st" ]]; then
-          	shift
-          	command git status "$@"
-                     elif [[ "$1" == "cm" ]]; then
-          	shift
-          	command git commit -am "$@"
-                     elif [[ "$1" == "br" ]]; then
-          	shift
-          	command git branch "$@"
-              else
-          	command git "$@"
-                     fi
-                 }
+          export ZSH_COMPDUMP="$ZSH_CACHE_DIR/zcompdump-$HOST-$ZSH_VERSION"
+
+          if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+            source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+          fi
+
+          source ${inputs.p10k-src}/powerlevel10k.zsh-theme
+          [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+          # Deine Yazi-Funktion bleibt unverändert
+          function y() {
+            local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+            command yazi "$@" --cwd-file="$tmp"
+            IFS= read -r -d "" cwd < "$tmp"
+            [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+            rm -f -- "$tmp"
+          }
         '';
-        ohMyZsh = {
-          enable = true;
-          plugins = ["git"];
-        };
       };
     };
   };
