480656f3c6cde29384a3289306e022ea70938b9b
Xeravus <cato.jenisch@gmail.com>
2026-07-21 17:45:38 +0200

    Auto-Rebuild: 2026-07-21 17:45:38

--- modules/essentials/shells.nix
+++ modules/essentials/shells.nix
@@ -54,31 +54,29 @@
             nix-pr = "nixpkgs-review pr --print-result";
           };
           interactiveShellInit = ''
-            zmodload zsh/zprof
-                             ZSH_CACHE_DIR="$HOME/.cache/zsh"
-                             if [[ ! -d "$ZSH_CACHE_DIR" ]]; then
-                               mkdir -p "$ZSH_CACHE_DIR"
-                             fi
+                   ZSH_CACHE_DIR="$HOME/.cache/zsh"
+                   if [[ ! -d "$ZSH_CACHE_DIR" ]]; then
+                     mkdir -p "$ZSH_CACHE_DIR"
+                   fi
 
-                             export ZSH_COMPDUMP="$ZSH_CACHE_DIR/zcompdump-$HOST-$ZSH_VERSION"
-                      export DIRENV_LOG_FORMAT=""
+                   export ZSH_COMPDUMP="$ZSH_CACHE_DIR/zcompdump-$HOST-$ZSH_VERSION"
+            export DIRENV_LOG_FORMAT=""
 
-                             if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-                               source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-                             fi
+                   if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+                     source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+                   fi
 
-                             source ${inputs.p10k-src}/powerlevel10k.zsh-theme
-                             [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+                   source ${inputs.p10k-src}/powerlevel10k.zsh-theme
+                   [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-                             # Deine Yazi-Funktion bleibt unverändert
-                             function y() {
-                               local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-                               command yazi "$@" --cwd-file="$tmp"
-                               IFS= read -r -d "" cwd < "$tmp"
-                               [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
-                               rm -f -- "$tmp"
-                             }
-            zprof
+                   # Deine Yazi-Funktion bleibt unverändert
+                   function y() {
+                     local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+                     command yazi "$@" --cwd-file="$tmp"
+                     IFS= read -r -d "" cwd < "$tmp"
+                     [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+                     rm -f -- "$tmp"
+                   }
           '';
         };
       };
