66dc148465a49cd2dabb082036224a16a35de519
Xeravus <cato.jenisch@gmail.com>
2026-08-04 15:26:56 +0200

    Auto-Rebuild: 2026-08-04 15:26:56

--- modules/desktop/lightdm.nix
+++ modules/desktop/lightdm.nix
@@ -12,8 +12,14 @@
     };
   };
 
-  config = lib.mkIf config.xanterella.lightdm.enable {
-    services.xserver.displayManager.lightdm.greeters.gtk.enable = true;
-    services.xserver.displayManager.lightdm.enable = true;
-  };
+  config = lib.mkMerge [
+    (lib.mkIf config.xanterella.lightdm.enable {
+      services.xserver.displayManager.lightdm.greeters.gtk.enable = true;
+      services.xserver.displayManager.lightdm.enable = true;
+    })
+    (lib.mkIf (!config.xanterella.lightdm.enable) {
+      services.xserver.displayManager.lightdm.greeters.gtk.enable = false;
+      services.xserver.displayManager.lightdm.enable = false;
+    })
+  ];
 }
