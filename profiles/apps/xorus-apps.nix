f3606bc6f23c9989b282f4258fc917bb4bf34c14
Xeravus <cato.jenisch@gmail.com>
2026-08-04 20:01:26 +0200

    Auto-Rebuild: 2026-08-04 20:01:26

---
+++ profiles/apps/xorus-apps.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  config = {
+    xanterella = {
+      browser = {
+        zen = {
+          enable = true;
+        };
+        librewolf = {
+          enable = true;
+        };
+      };
+      brightnessctl = {
+        enable = true;
+      };
+      fastfetch = {
+        enable = true;
+      };
+      spicetify = {
+        enable = true;
+      };
+    };
+  };
+}
