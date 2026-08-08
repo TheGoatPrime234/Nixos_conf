aa2ae3926aa32679b6ab666af307416f1851c4f4
Xeravus <cato.jenisch@gmail.com>
2026-08-08 23:24:00 +0200

    Auto-Rebuild: 2026-08-08 23:24:00

--- modules/desktop/noctalia.nix
+++ modules/desktop/noctalia.nix
@@ -5,51 +5,32 @@
   inputs,
   ...
 }: let
+  cfg = config.xanterella.noctalia;
+
   noctaliaConfigFile = ./noctalia.toml;
-  fakeConfigDir = pkgs.runCommand "noctalia-fake-config-dir" {} ''
-    mkdir -p $out/noctalia
-    cp ${noctaliaConfigFile} $out/noctalia/config.json
-  '';
-  noctaliaWrapped = pkgs.symlinkJoin {
-    name = "noctalia-wrapped";
-    paths = [inputs.noctalia.packages.${pkgs.system}.default];
-    buildInputs = [pkgs.makeWrapper];
-    postBuild = ''
-      wrapProgram $out/bin/noctalia \
-        --set XDG_CONFIG_HOME ${fakeConfigDir}
-    '';
-  };
 in {
   options = {
     xanterella = {
       noctalia = {
-        enable = lib.mkEnableOption "Aktiviert den deklarativen Noctalia-Wrapper";
-      };
-      noctalia_vimjoyer = {
-        enable = lib.mkEnableOption "";
+        enable = lib.mkEnableOption "Aktiviert Noctalia deklarativ über tmpfiles";
       };
     };
   };
 
-  config = lib.mkMerge [
-    (lib.mkIf config.xanterella.noctalia.enable {
-      environment = {
-        systemPackages = [
-          noctaliaWrapped
-        ];
-        etc = {
-          "wallpaper" = {
-            source = inputs.wallpaper;
-          };
+  config = lib.mkIf cfg.enable {
+    environment = {
+      etc = {
+        "wallpaper" = {
+          source = inputs.wallpaper;
         };
       };
-    })
-    (lib.mkIf config.xanterella.noctalia_vimjoyer.enable {
-      #packages.noctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
-      #  settings =
-      #    (builtins.fromTOML
-      #      (builtins.readFile ./noctalia.toml)).settings;
-      #};
-    })
-  ];
+      systemPackages = [
+        inputs.noctalia.packages.${pkgs.system}.default
+      ];
+    };
+    systemd.user.tmpfiles.rules = [
+      "d %h/.config/noctalia 0755 - - -"
+      "L+ %h/.config/noctalia/config.toml - - - - ${noctaliaConfigFile}"
+    ];
+  };
 }
