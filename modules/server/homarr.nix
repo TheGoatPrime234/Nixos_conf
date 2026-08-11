b2951bc404b42d619288b12345fe9d34ddb30a26
Xeravus <cato.jenisch@gmail.com>
2026-08-11 23:49:54 +0200

    Auto-Rebuild: 2026-08-11 23:49:54

--- modules/server/homarr.nix
+++ modules/server/homarr.nix
@@ -54,7 +54,7 @@
           "https://${config.xanterella.homarr.domain}" = {
             extraConfig = ''
                           handle /home* {
-                            reverse_proxy ${config.virtualisation.oci-containers.containers.homarr.ports}
+                            reverse_proxy 127.0.0.1:7575
               }
             '';
           };
