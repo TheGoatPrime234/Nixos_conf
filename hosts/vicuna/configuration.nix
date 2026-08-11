7cbf6f722896fee430ae8a80818206b5724bcda2
Xeravus <cato.jenisch@gmail.com>
2026-08-11 22:52:12 +0200

    Auto-Rebuild: 2026-08-11 22:52:12

--- hosts/vicuna/configuration.nix
+++ hosts/vicuna/configuration.nix
@@ -19,6 +19,30 @@
   };
   sdImage = {
     firmwareSize = 1024;
+    populateFirmwareCommands = lib.mkForce ''
+      # Pfad zum fertig kompilierten NixOS-System
+      systemdir=${config.system.build.toplevel}
+
+      # Originale Raspberry Pi Firmware kopieren (start.elf, bootcode.bin etc.)
+      cp -r ${pkgs.raspberrypifw}/share/raspberrypi/boot/* firmware/
+
+      # Den echten NixOS-Kernel und die RAM-Disk umbenennen und kopieren
+      cp $systemdir/kernel firmware/kernel_2712.img
+      cp $systemdir/initrd firmware/initrd
+
+      # Die config.txt exakt auf den Pi 5 zuschneiden
+      cat <<EOF > firmware/config.txt
+      [pi5]
+      kernel=kernel_2712.img
+      initramfs initrd followkernel
+      arm_64bit=1
+      EOF
+
+      # Dem Kernel sagen, wo das NixOS-Dateisystem liegt
+      cat <<EOF > firmware/cmdline.txt
+      console=tty0 root=/dev/disk/by-label/NIXOS_SD rootfstype=ext4 rootwait init=$systemdir/init
+      EOF
+    '';
   };
   hardware = {
     #bluetooth = { enable = false; };
