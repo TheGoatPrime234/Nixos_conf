ae114e2ead2b78e483d0e7a4cbc8a8a76245b24c
Xeravus <cato.jenisch@gmail.com>
2026-08-15 20:43:44 +0200

    Auto-Rebuild: 2026-08-15 20:43:44

--- modules/server/github-runner.nix
+++ modules/server/github-runner.nix
@@ -1,7 +1,7 @@
 {
   config,
   lib,
-  pkgs,
+  pkgs-new,
   ...
 }: let
   cfg = config.xanterella.github-runner;
@@ -41,7 +41,7 @@ in {
           tokenFile = config.age.secrets.github-runner-token.path;
           extraLabels = runnerCfg.labels;
           replace = true;
-          extraPackages = with pkgs; [
+          extraPackages = with pkgs-new; [
             git
             nodejs
             bash
