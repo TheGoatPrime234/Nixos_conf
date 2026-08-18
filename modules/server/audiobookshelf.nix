ea15faa8524139829311c2f3134cc66d3b92fff2
Xeravus <cato.jenisch@gmail.com>
2026-08-18 17:13:35 +0200

    Auto-Rebuild: 2026-08-18 17:13:35

--- modules/server/audiobookshelf.nix
+++ modules/server/audiobookshelf.nix
@@ -42,10 +42,10 @@ in {
     systemd = {
       tmpfiles = {
         rules = [
-          "d /mnt/server-data/audiobookshelf 0755 audiobookshelf audiobookshelf -"
-          "d /mnt/server-data/audiobookshelf/config 0755 audiobookshelf audiobookshelf -"
-          "d /mnt/server-data/audiobookshelf/metadata 0755 audiobookshelf audiobookshelf -"
-          "d /mnt/server-data/audiobookshelf/audiobookshelf 0755 audiobook audiobookshelf -"
+          "d /mnt/server-data/audiobookshelf 0755 root root -"
+          "d /mnt/server-data/audiobookshelf/config 0755 root root -"
+          "d /mnt/server-data/audiobookshelf/metadata 0755 root root -"
+          "d /mnt/server-data/audiobookshelf/audiobooks 0755 root root -"
         ];
       };
     };
