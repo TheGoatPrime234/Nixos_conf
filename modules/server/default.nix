a8b142c523b3fd162257b90005e8d0088913f851
Xeravus <cato.jenisch@gmail.com>
2026-08-19 22:54:18 +0200

    Auto-Rebuild: 2026-08-19 22:54:18

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./cluster-node.nix
+    ./metasploitable.nix
     ./prometheus.nix
     ./audiobookshelf.nix
     ./immich.nix
---
+++ modules/server/metasploitable.nix
@@ -0,0 +1,56 @@
+{
+  config,
+  pkgs,
+  lib,
+  pkgs-unstable,
+  ...
+}: let
+  cfg = config.xanterella.metasploitable;
+  nodeCfg = config.xanterella.cluster-node;
+in {
+  options = {
+    xanterella = {
+      metasploitable = {
+        enable = lib.mkEnableOption "Aktiviert Vaultwarden ohne externes Speichermedium";
+        domain = lib.mkOption {
+          type = lib.types.str;
+          default = "${nodeCfg.domain}:1003";
+        };
+      };
+    };
+  };
+
+  config = lib.mkIf (cfg.enable && nodeCfg.enable) {
+    networking = {
+      interfaces = {
+        metasploit-net = {
+          virtual = true;
+          ipv4 = {
+            addresses = [
+              {
+                address = "10.99.99.1";
+                prefixLength = 24;
+              }
+            ];
+          };
+        };
+      };
+    };
+    virtualisation = {
+      oci-containers = {
+        backend = "podman";
+        containers = {
+          metasploitable = {
+            image = "tleemcjr/metasploitable2:latest";
+            autoStart = false;
+            ports = [
+              "10.99.99.1:21:21"
+              "10.99.99.1:22:22"
+              "10.99.99.1:80:80"
+            ];
+          };
+        };
+      };
+    };
+  };
+}
