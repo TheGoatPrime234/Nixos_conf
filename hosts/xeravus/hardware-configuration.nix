4b50e64d65b705b8b24911c158dca52caf3f931b
Xeravus <cato.jenisch@gmail.com>
2026-08-30 13:52:08 +0200

    Auto-Rebuild: 2026-08-30 13:52:08

--- hosts/xeravus/hardware-configuration.nix
+++ hosts/xeravus/hardware-configuration.nix
@@ -13,7 +13,7 @@
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
-  boot.kernelParams = ["i8042.nopnp=1"];
+  boot.kernelParams = ["i8042.reset" "i8042.nomux=1" "i8042.nopnp=0"];
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/d53b5516-59f1-4ec1-a7b2-03549ebf5a59";
